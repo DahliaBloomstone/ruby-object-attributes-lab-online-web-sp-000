dafbdeee03dd7bd9a05a70d4a24b5f74e7475bf6
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-22 21:06:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
 
   def name=(dogs_name)
-    @name = dogs_name
+    @name = dogs_name  #writes the name of the dog to an instance variable @name
   end
 
   def name
-    @name
+    @name       reads the name of the dog from an instance variable @name
   end
   def breed=(breed)
     @breed = breed
