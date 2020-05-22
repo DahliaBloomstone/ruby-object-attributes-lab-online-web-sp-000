d644e3a3df1869ac2622a7e78743f87a745f8e44
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-22 21:03:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
 class Dog
-def initialize(name)
-  @name = name
-end
-
-def name
-  @name
-end
-
-
-class Dog
-def breed
-  @breed
-end
-
-
+  
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+  def name
+    @name 
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end 
 end
